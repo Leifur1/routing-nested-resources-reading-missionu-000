727e248ea3d3457516000184f8ef33f707014e0a
Leifur Sigurdsson <leifurerik@gmail.com>
2019-02-26 23:12:38 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'authors/:id/posts'
-  get 'authors/:id/posts/:post_id'
   resources :authors, only: [:show]
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
